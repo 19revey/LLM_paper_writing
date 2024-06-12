d19ea3e7a4cd8f0fdb16c3847bc4aee897366fca
yduan92 <yduan92@gatech.edu>
2024-06-12 09:13:15 -0700

    udpate

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ import streamlit as st
 from src.llm import llm
 
 
-st.title("Improve your scientific writing using LLM")
+st.title("Enhance your scientific writing with LLM")
 st.text("powered by Gemini pro, last updated on 2024-06-11")
 
-st.text("Enhance your writing using RAG.")
+st.text("Retrieval-augmented generation based on over 100 papers in your field")
 
 context=st.text_area("Paste the text here",help="Please paste the text here")
 # uploaded_file=st.file_uploader("Upload Your Resume",type="pdf",help="Please uplaod the pdf")
