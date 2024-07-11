9c92a24167daccd9fd06630ffccf0afd937f8e64
Y. Duan <duanyifeixp@gmail.com>
2024-07-11 14:43:50 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from src.llm import llm
 
 
 st.title("Enhance your scientific writing with domain-specific LLM")
-st.text("- powered by Gemini pro, last updated on 2024-06-11")
-
+# st.text("- powered by Gemini pro, last updated on 2024-06-11")
+st.text("- Made by Yifei, last updated on 2024-06-11")
 st.text("Rephrase your draft or answer technical questions \n based on thousands of papers in the field")
 
 context=st.text_area("Paste the text here",help="Please paste the text here")
