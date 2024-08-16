9c484739ada71ff43c01be61b1f35b1e10fc2275
yduan92 <yduan92@gatech.edu>
2024-08-15 21:03:29 -0700

    update vectore store

--- app.py
+++ app.py
@@ -8,19 +8,21 @@ from src.granularsegregation import segregation
 st.title("Enhance your scientific writing with domain-specific LLM")
 # st.text("- powered by Gemini pro, last updated on 2024-06-11")
 st.text("- Made by Yifei, last updated on 2024-07-17")
-st.text("Rephrase your draft or answer technical questions in the field of granular segregation. \n Support for other fields will be available later.")
+st.text("Rephrase your draft or answer technical questions in the field of choice. \n Support for other fields will be available later.")
 
 # llm = llm(model="gemini-1.5-pro",temperature=0.5)
 llm = llm(temperature=0.5)
 
 # VectorStore selection
-VectorStore = ["Granular Segregation","Membrane"]  # You can dynamically load folder names if needed
+VectorStore = ["Granular Segregation","Membrane Science","Cyper Security"]  # You can dynamically load folder names if needed
 selected_VectorStore = st.selectbox("Select a Topic", VectorStore)
 
 if selected_VectorStore == "Granular Segregation":
     reply=segregation(llm)
 elif selected_VectorStore == "Membrane":
     reply=llm.get_llm_response(vecstore="membrane")
+elif selected_VectorStore == "Cyper Security":
+    reply=llm.get_llm_response(vecstore="cyber_security")
 
 # Generate content
 context=st.text_area("Paste the text here",help="Please paste the text here")
